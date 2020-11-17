f20cb6ed893e9cebba46d400464e83f0283692dc
tdj-kevincetti <timezainw2000@gmail.com>
2020-11-17 11:20:42 +0700

    2 files modified, 1 added

---
+++ src/app/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebaseConfig : {
+    apiKey: "AIzaSyD1ZCaPkLFciarnvOtoc1RGkGi7oZHhlVk",
+    authDomain: "francispd-a3dbd.firebaseapp.com",
+    databaseURL: "https://francispd-a3dbd.firebaseio.com",
+    projectId: "francispd-a3dbd",
+    storageBucket: "francispd-a3dbd.appspot.com",
+    messagingSenderId: "940511091484",
+    appId: "1:940511091484:web:96562e0b42064ad70838d9",
+    measurementId: "G-JDY1EV0BKT"
+  };
+}
+
