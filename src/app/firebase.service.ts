f2644584857fdeb14ed8afb829254e40d48b75f8
tdj-kevincetti <timezainw2000@gmail.com>
2020-11-17 13:36:55 +0700

    1 file modified

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -8,8 +8,25 @@ import {Tweet} from './tweet';
 )
 export class FirebaseService {
 
-  constructor(
-    private firestore: AngularFirestore
-  ) { }
+  constructor(private firestore: AngularFirestore) { }
+  getTweets(){
+    let docref = this.firestore.collection<Tweet>("tweet",e=>e.orderBy("date","desc"))
+    return docref.valueChanges();
+  }
+  addTweet(n:string,m:string){
+    let tweet = { name:n,
+    msg:m,
+    date:firebase.default.firestore.Timestamp.now()
+    }
+    const ref = this.firestore.collection('tweet').add(tweet);
+    ref.then(newRef=>{
+      const updateID = {
+        id: newRef.id
+      };
+      newRef.update(updateID)
+    })
+    return ref
+  }
+  
 
 }
