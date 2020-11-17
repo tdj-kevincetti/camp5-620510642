1b7c1273c923f12740158558d65c20bd143f69f0
tdj-kevincetti <timezainw2000@gmail.com>
2020-11-17 09:45:49 +0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+//modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
+//firebase
+
+//component
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { AddTweetComponent } from './add-tweet/add-tweet.component';
 import { TimeLineComponent } from './time-line/time-line.component';
 import { HomeComponent } from './home/home.component';
-
+//service
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, AddTweetComponent, TimeLineComponent, HomeComponent ],
+  imports:      [ BrowserModule, FormsModule, RouterModule.forRoot([
+    {path: "time-line",component: TimeLineComponent},
+    {path: "add-tweet",component: AddTweetComponent},
+  ]) 
+  ],
+  declarations: [ 
+    AppComponent, 
+    HelloComponent, 
+    AddTweetComponent, 
+    TimeLineComponent, 
+    HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
