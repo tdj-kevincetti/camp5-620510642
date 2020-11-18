56ad884db13113a364994c880fa94eb54fcad72d
tdj-kevincetti <timezainw2000@gmail.com>
2020-11-18 09:32:38 +0700

    1 file modified

--- src/app/display-tweet/display-tweet.component.ts
+++ src/app/display-tweet/display-tweet.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Input } from '@angular/core'
+import { Tweet } from '../tweet'
 @Component({
   selector: 'app-display-tweet',
   templateUrl: './display-tweet.component.html',
@@ -8,8 +9,35 @@ import { Component, OnInit } from '@angular/core';
 export class DisplayTweetComponent implements OnInit {
 
   constructor() { }
-
+  @Input() tweet: Tweet;
   ngOnInit() {
+    this.tweet = {
+      ...this.tweet,
+      date: this.timeAgo(this.tweet.date.toDate())
+    }
+    
+  }
+  timeAgo(val: Date){
+    const now = new Date();
+    const diff = Math.abs(now.getTime() - val.getTime());
+    const diffDay = Math.ceil(diff / (1000* 3600 *24));
+    const diffHour = Math.ceil(diff / (1000 * 3600));
+    const diffMinute = Math.ceil(diff / (1000 * 60));
+    const diffSecond = Math.ceil(diff / 1000);
+
+    if(diffSecond < 60){
+      return "Just now.";
+    }
+
+    if(diffMinute < 60){
+      return `${diffMinute} minute(s) ago.`;
+    }
+
+    if(diffHour < 24){
+      return `${diffHour} hour(s) ago.`;
+    }
+
+    return `${diffDay} day(s) ago.`;
   }
 
 }
