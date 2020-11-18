870302cd486b1cbe4637eadc73b487ef5bc2bc27
tdj-kevincetti <timezainw2000@gmail.com>
2020-11-18 10:33:24 +0700

    2 files modified

--- src/app/add-tweet/add-tweet.component.ts
+++ src/app/add-tweet/add-tweet.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl,FormGroup} from '@angular/forms';
 import {FirebaseService} from '../firebase.service'
+import {Router} from '@angular/router'
 @Component({
   selector: 'app-add-tweet',
   templateUrl: './add-tweet.component.html',
@@ -11,9 +12,16 @@ export class AddTweetComponent implements OnInit {
     name: new FormControl(''),
     msg: new FormControl('')
   })
-  constructor() { }
+  constructor(private firebaseService:FirebaseService,
+  private router:Router) { }
 
   ngOnInit() {
   }
-
+  onTweet(){
+    this.firebaseService.addTweet(
+      this.form.value.name,
+      this.form.value.msg
+    )
+    this.router.navigate(['time-line'])
+  }
 }
